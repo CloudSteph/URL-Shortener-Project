5e6f7bb68ba50a26db19be2b5ddf18255dcc165f
Stephanie Liew <liewstephanie88@gmail.com>
2025-02-06 13:16:49 -0500

    Add form for URL input and generate short URLs

--- app.py
+++ app.py
@@ -15,4 +15,24 @@ def generate_short_code(length=6):
 
 # Define a route for the homepage "/"
 @app.route('/', methods=['GET', 'POST'])
-def home
+def home():
+    if request.method == 'POST':
+        long_url = request.form['long_url']
+        short_code = generate_short_code()
+        url_db[short_code] = long_url
+        short_url = request.host_url + short_code
+        return f'Welcome to the URL Shortener!"<br>Shortened URL: <a href="{short_url}">{short_url}</a>'
+
+    # Display form with welcome message
+    return '''
+        <h1>Welcome to the URL Shortener!</h1>
+        <form method="post">
+            Enter URL: <input type="text" name="long_url">
+            <input type="submit" value="Shorten">
+        </form>
+    '''
+
+
+# Run the Flask app when the script is executed
+if __name__ == '__main__':
+    app.run(debug=True)
